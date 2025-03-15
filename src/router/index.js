d918e0478fd36e639aea74b473a57d8cdbf54d23
babemonkey <799785425@qq.com>
2025-03-15 23:56:44 +0800

    路由设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-// 添加home路由配置（如果尚未存在）
-const routes = [
-  {
-    path: '/',
-    redirect: '/home' // 添加根路径重定向
-  },
-  { // 添加/order-ease-iui/home路由配置
-    path: '/order-ease-iui/home',
-    name: 'OrderEaseHome',
-    component: () => import('../views/home/index.vue')
-  },
-  { // 添加/order-ease-iui/orders路由配置
-    path: '/order-ease-iui/orders',
-    name: 'OrderEaseOrders',
-    component: () => import('../views/Orders.vue')
-  },
-  {
-    path: '/mine',
-    name: 'Mine',
-    component: () => import('../views/Mine.vue')
-  }
-]
-
+// 修改路由配置
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHistory('/order-ease-iui/'), // 设置基础路径
+  routes: [
+    {
+      path: '/', // 匹配部署路径根目录
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: () => import('../views/home/index.vue')
+    },
+    { // 添加/order-ease-iui/orders路由配置
+      path: '/order-ease-iui/orders',
+      name: 'OrderEaseOrders',
+      component: () => import('../views/Orders.vue')
+    },
+    {
+      path: '/mine',
+      name: 'Mine',
+      component: () => import('../views/Mine.vue')
+    }
+  ]
 })
 
 export default router
