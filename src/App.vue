4b0a1230fb244e51a94a16d612264933ebf55240
babemonkey <799785425@qq.com>
2025-03-16 18:04:17 +0800

    修复路由问题

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div class="app-container">
     <router-view></router-view>
     <van-tabbar v-model="active" fixed>
-      <van-tabbar-item icon="shop-o" to="/order-ease-iui/home">首页</van-tabbar-item>
-      <van-tabbar-item icon="orders-o" to="/order-ease-iui/orders">订单</van-tabbar-item>
+      <van-tabbar-item icon="shop-o" to="/home">首页</van-tabbar-item>
+      <van-tabbar-item icon="orders-o" to="/orders">订单</van-tabbar-item>
       <van-tabbar-item icon="user-o" to="/mine">我的</van-tabbar-item>
     </van-tabbar>
   </div>
